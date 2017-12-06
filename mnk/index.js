78c9f8d933040fb927a9d15e9132429ac11d96a8
erikwilson <erik.e.wilson@gmail.com>
2017-12-06 14:43:12 -0700

    classy

--- mnk/index.js
+++ mnk/index.js
@@ -4,67 +4,59 @@ const DeepLearnPlayer = require('./players/deeplearn')
 const HumanPlayer  = require('./players/human')
 const BrutePlayer  = require('./players/brute')
 
-const m = 3
-const n = 3
-const k = 3
-
-const PlayerTypes = {
-  random: new RandomPlayer(),
-  human: new HumanPlayer(),
-  brute: new BrutePlayer({m,n,k}),
-}
-
-const dlps = new Array(2).fill(0)
-for (let i in dlps) {
-  dlps[i] = new DeepLearnPlayer()
-  PlayerTypes[`dlp${i}`] = dlps[i]
-}
-const players = [ PlayerTypes.brute, PlayerTypes.brute ]
-
-const game = new Game({m,n,k,players})
-let stats = {}
-const play = (count=1, done=console.log) => {
-  let startTime = Date.now()
-  let replay = (count=1) => {
-    if (count===0) {
-      const finishTime = Date.now()
-      const timeMs = finishTime-startTime
-      stats.timeMs = timeMs
-      done(stats)
-      stats = {}
-      console.log({finishTime,startTime,timeMs, match:startTime+timeMs===finishTime})
-      startTime = Date.now()
-      return
-    }
-//
-    game.reset()
-    setTimeout(() => game.play(async (results) => {
-      let { winner } = results
-      winner = winner || 'tie'
-      if (!stats[winner]) stats[winner]=1
-      else stats[winner]++
-      // console.log('winner:',winner,history)
-      // for (let dlp of dlps) await dlp.learnGame(results)
-      replay(count-1)
-    }),0)
+class MNK {
+  constructor(options={m:3,n:3,k:3}) {
+    this.setup(options)
   }
-  replay(count)
+
+  setup({m,n,k}) {
+    const PlayerTypes = this.PlayerTypes = {
+      random: new RandomPlayer(),
+      human: new HumanPlayer(),
+      brute: new BrutePlayer({m,n,k}),
+    }
+
+    const dlps = new Array(2).fill(0)
+    for (let i in dlps) {
+      dlps[i] = new DeepLearnPlayer()
+      PlayerTypes[`dlp${i}`] = dlps[i]
+    }
+
+    this.stats = {}
+    this.m = 3
+    this.n = 3
+    this.k = 3
+    const players = this.players = [ PlayerTypes.brute, PlayerTypes.brute ]
+    this.game = new Game({m,n,k,players})
+  }
+
+  play(count=1, done=console.log) {
+    let startTime = Date.now()
+    let replay = (count=1) => {
+      if (count===0) {
+        const finishTime = Date.now()
+        const timeMs = finishTime-startTime
+        this.stats.timeMs = timeMs
+        done(this.stats)
+        this.stats = {}
+        console.log({finishTime,startTime,timeMs, match:startTime+timeMs===finishTime})
+        startTime = Date.now()
+        return
+      }
+  //
+      this.game.reset()
+      setTimeout(() => this.game.play(async (results) => {
+        let { winner } = results
+        winner = winner || 'tie'
+        if (!this.stats[winner]) this.stats[winner]=1
+        else this.stats[winner]++
+        // console.log('winner:',winner,history)
+        // for (let dlp of dlps) await dlp.learnGame(results)
+        replay(count-1)
+      }),0)
+    }
+    replay(count)
+  }
 }
-//
-// (async () => {
-//   const DeepLearn = require('./mnk/players/deeplearn')
-//   const p = new DeepLearn()
-//   p.train([[0,0,0,1,0,0,0,0,0]],[[0,0,0,1,0,0,0,0,0]])
-//   const r = await p.run([0,0,0,1,0,0,0,0,0]).getValues()
-//   console.log(r)
-// })()
 
-const context = { play, players, PlayerTypes }
-
-module.exports = context
-
-// if (module && module.exports) {
-//   module.exports = replay
-// } else {
-//   replay()
-// }
+module.exports = new MNK()
